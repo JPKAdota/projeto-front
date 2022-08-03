3299aa76bd97bc18ee317aa73b3c534149225ca3
Kadota <joaokadota@gmail.com>
2022-08-02 23:16:01 -0300

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   angular.json
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/app/app.component.html
    	modified:   src/app/app.module.ts
    	new file:   src/app/components/book-cart/book-cart.component.css
    	new file:   src/app/components/book-cart/book-cart.component.html
    	new file:   src/app/components/book-cart/book-cart.component.spec.ts
    	new file:   src/app/components/book-cart/book-cart.component.ts
    	new file:   src/app/components/book-cart/filters/filters.component.css
    	new file:   src/app/components/book-cart/filters/filters.component.html
    	new file:   src/app/components/book-cart/filters/filters.component.spec.ts
    	new file:   src/app/components/book-cart/filters/filters.component.ts
    	new file:   src/app/components/book-cart/product-list/product-list.component.css
    	new file:   src/app/components/book-cart/product-list/product-list.component.html
    	new file:   src/app/components/book-cart/product-list/product-list.component.spec.ts
    	new file:   src/app/components/book-cart/product-list/product-list.component.ts
    	new file:   src/app/components/shared/footer/footer.component.css
    	new file:   src/app/components/shared/footer/footer.component.html
    	new file:   src/app/components/shared/footer/footer.component.spec.ts
    	new file:   src/app/components/shared/footer/footer.component.ts
    	new file:   src/app/components/shared/header/header.component.css
    	new file:   src/app/components/shared/header/header.component.html
    	new file:   src/app/components/shared/header/header.component.spec.ts
    	new file:   src/app/components/shared/header/header.component.ts
    	new file:   src/app/components/shared/nav/nav.component.css
    	new file:   src/app/components/shared/nav/nav.component.html
    	new file:   src/app/components/shared/nav/nav.component.spec.ts
    	new file:   src/app/components/shared/nav/nav.component.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { BookCartComponent } from './components/book-cart/book-cart.component';
+import { ProductListComponent } from './components/book-cart/product-list/product-list.component';
+import { FiltersComponent } from './components/book-cart/filters/filters.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    BookCartComponent,
+    ProductListComponent,
+    FiltersComponent
   ],
   imports: [
     BrowserModule,
